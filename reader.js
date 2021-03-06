386fd0ac1b4d194dcf7a52e462cf0011f2d7f15c
HAPPY <61936234+HAPPY-21XIII@users.noreply.github.com>
2021-03-06 20:35:13 +0300

    Update reader.js

--- reader.js
+++ reader.js
@@ -1,24 +1,25 @@
 const fs = require("fs");
 
 module.exports = bot => {
+  console.log("┌──────────────────────────────────────────────────────────────┐")
   fs.readdir("./events/", (err, files) => {
     if (err) console.error(err);
     let jsfiles = files.filter(f => f.split(".").pop() === "js");
-    if (jsfiles.length <= 0) return console.log("Herhangi bir event dosyası bulunamadı!");
-    console.log(`• ${jsfiles.length} adet event yükleniyor...`);
+    if (jsfiles.length <= 0) return console.log("│ [SİSTEM] Herhangi bir event dosyası bulunamadı!");
+    console.log(`│ [SİSTEM] ${jsfiles.length} ADET EVENT TARANIYOR...`);
     jsfiles.forEach((f, i) => {
       require(`./events/${f}`);
-      console.log(`${f} dosyası yüklendi!`);
+      console.log(`│ [EVENTS] Taranan Dosya: ${f}`);
     });
   });
   fs.readdir("./commands/", (err, files) => {
     if (err) console.log(err);
     let jsfile = files.filter(f => f.split(".").pop() === "js");
-    if (jsfile.lenght <= 0)return console.log("Herhangi bir komut dosyası bulunamadı!");
-    console.log(`• ${jsfile.length} adet komut yükleniyor...`);
+    if (jsfile.lenght <= 0)return console.log("│ [SİSTEM] Herhangi bir komut dosyası bulunamadı!");
+    console.log(`│ [SİSTEM]  ${jsfile.length} ADET KOMUT TARANIYOR...`);
     jsfile.forEach((f, i) => {
       let props = require(`./commands/${f}`);
-      console.log(`${f} dosyası yüklendi!`);
+      console.log(`│ [KOMUT] Taranan dosya: ${f}`);
       bot.commands.set(props.help.name, props);
       props.conf.aliases.forEach(alias => {
         bot.aliases.set(alias, props.help.name);
